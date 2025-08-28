d4e9a8f04c0a030b0ee3b3953732f8c46fb946c4
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2025-08-28 16:44:22 +0530

    Update PersonRepo.java

--- SpringBoot/SpringBootInheritanceMapping/src/main/java/com/company/repo/PersonRepo.java
+++ SpringBoot/SpringBootInheritanceMapping/src/main/java/com/company/repo/PersonRepo.java
@@ -12,7 +12,15 @@ import com.company.entity.base.Person;
 
 @Repository
 public interface PersonRepo extends JpaRepository<Person , Long>{
+	// use existing findAll methods from JPA to display student and teacher details using person in a polymorphic behaviour
+	//Polymorphic Behaviour : To query with Parent  and you will get all childs automatically
+
+	// JPQL
+	@Query("select s from Student s")
+	List<Student> findAllStudents();
 	
+	@Query("select t from Teacher t")
+	List<Teacher>findAllTeachers();
 	// JPQL
 			@Query("select s from Student s")
 			List<Student> findAllStudents();
@@ -21,3 +29,4 @@ public interface PersonRepo extends JpaRepository<Person , Long>{
 			List<Teacher>findAllTeachers();
 
 }
+
