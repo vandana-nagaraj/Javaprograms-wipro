cc726783feee2dbfd284b87a556cf5f4acc2f843
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2025-08-28 12:43:45 +0530

    Add files via upload

---
+++ SpringBoot/SpringBootInheritanceMapping/src/main/java/com/company/repo/StudentRepo.java
@@ -0,0 +1,15 @@
+package com.company.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.company.entity.Student;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student , Long>{
+	
+	List<Student> findAll();
+
+}
