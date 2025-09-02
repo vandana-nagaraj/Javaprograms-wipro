fcef683c8f0d4e2edfa8d0b24b25519b0374f6f5
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2025-09-02 17:43:55 +0530

    Update UserService.java

--- SpringBoot/ValidationExample/src/main/java/com/example/validation/service/UserService.java
+++ SpringBoot/ValidationExample/src/main/java/com/example/validation/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.validation.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,45 @@ public class UserService {
 	
 	public List<User> getAllUsers()
 	{
-		return userRepo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+	
+	
+	public Optional<User> getUserById(Long id)
+	{
+		return userRepo.findById(id);
+	}
+	
+	public User updateUserById(Long id, User user)
+	{
+		User user1 = null;
+		Optional<User> ouser = userRepo.findById(id);
+		
+		if(ouser.isPresent())
+		{
+			user1 = userRepo.save(user);
+		}
+		return user1;
+	}
+	
+	public User partialUpdateUserById(Long id , User user)
+	{
+		User user1 = null;
+		Optional<User> ouser = userRepo.findById(id);
+		
+		if(ouser.isPresent())
+		{
+			user1 = userRepo.save(user);
+		}
+		return user1;
+	}
+	
+	public void deleteUserById(Long id) throws Exception
+	{
+		
+		Optional<User> u = userRepo.findById(id);
+		User user = u.orElseThrow(() -> new Exception("user with this id does not exist" + id));
+	     userRepo.delete(user);
 	}
 
 }
